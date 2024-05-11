89af4583dc82e59db2faeb433259adae0c92e9e5
Marlow Haspert <marlowhaspert@gmail.com>
2024-05-11 18:37:54 -0300

    Enemy first pass

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,18 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private const string PlayerTag = "Player";
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (other.tag == PlayerTag)
+        {
+            
+        }
     }
 }
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,14 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Spawner : MonoBehaviour
 {
-    [SerializeField] private Enemy enemigo;
-    
-    // Update is called once per frame
+    [SerializeField] private Enemy[] prefabEnemigo;
+
+    [SerializeField] private float spawnTime;
+
+    [SerializeField]
+    [Range(0, 1)] private float spawnChance;
+
+    private float ultimoSpawnTime = 0;
+
+    private void Start()
+    {
+        transform.position = new Vector3(Screen.width + 3, transform.position.y, transform.position.z);
+    }
+
     void Update()
     {
-        
+        if (Time.time - ultimoSpawnTime > spawnTime)
+        {
+            if (Random.Range(0, 1) <= spawnChance)
+            {
+                Instantiate(prefabEnemigo[Random.Range(0, prefabEnemigo.Length)], transform.position, Quaternion.identity);
+                ultimoSpawnTime = Time.time;
+            }
+        }
     }
 }
